597f6fe7ccb45388c682ebc7a80210146ffdf803
Mafe Sanchez <mafesanchez0326@gmail.com>
2021-02-24 16:45:56 +0000

    PlayerMove

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # panic-practice
+PLAN:
+Dreamer:
+Realistic:
+Critic:
+
+
+
+
+TODO:
+
+Set frame or scene to 50 or a limit so character doesn't move over it. 
+ to get it backdrop
+
+
--- Unity Game Learnathon/Assets/Scripts/Player.cs
+++ Unity Game Learnathon/Assets/Scripts/Player.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private Vector2 targetPos;
+    public float Moveup;
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            targetPos = new Vector2(transform.position.x, transform.position.y + Moveup);
+            transform.position = targetPos;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Moveup);
+            transform.position = targetPos;
         
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
