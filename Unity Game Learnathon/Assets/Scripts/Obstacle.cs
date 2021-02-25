afe27154ab7f849a7f1faf49cc75e7c797a8c2bf
Jenn <jenndoyle@hotmail.co.uk>
2021-02-25 10:17:09 +0000

    obstacle

---
+++ Unity Game Learnathon/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+
+    public float speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //player takes damage
+            //need to add public int health * 3; to player script
+            //add health to player inspector
+            //add player tag to player on inspector
+            other.GetComponent<Player>().health -= damage;
+            Debug.Log(other.GetComponent<Player>().health);
+            Destroy(gameObject);
+
+        }
+    }
+
+
+}
