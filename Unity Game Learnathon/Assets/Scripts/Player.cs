df83b9c5456cea19256ce6eaff9869497540ced8
robertoface <reynolds.37@gmail.com>
2021-02-25 09:46:05 +0000

    player TimeDeltaTime stuff

--- Unity Game Learnathon/Assets/Scripts/Player.cs
+++ Unity Game Learnathon/Assets/Scripts/Player.cs
@@ -7,18 +7,23 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     private Vector2 targetPos;
     public float Moveup;
+    public float speed; 
+
     private void Update()
     {
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
 
             targetPos = new Vector2(transform.position.x, transform.position.y + Moveup);
-            transform.position = targetPos;
+            // transform.position = targetPos;
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             targetPos = new Vector2(transform.position.x, transform.position.y - Moveup);
-            transform.position = targetPos;
+            // tran sform.position = targetPos;
         
         }
     }
