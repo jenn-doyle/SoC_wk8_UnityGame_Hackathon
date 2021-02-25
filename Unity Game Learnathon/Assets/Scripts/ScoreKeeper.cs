cf9247479632749eb789b346a661c881b41c0117
Mafe Sanchez <mafesanchez0326@gmail.com>
2021-02-25 16:43:15 +0000

    scoreKeeper done

--- Unity Game Learnathon/Assets/Scripts/ScoreKeeper.cs
+++ Unity Game Learnathon/Assets/Scripts/ScoreKeeper.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreKeeper : MonoBehaviour
 {
     public int score;
-    // Start is called before the first frame update
+    public Text scoreDisplay;
+    private void Update()
+    {
+        scoreDisplay.text = score.ToString();
+    }
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Obstacle"))
